085bd9d786d9c11ed3588450d842544c32b8729d
LeoTao777 <1299689745@qq.com>
2026-03-18 22:21:04 +0800

    feat:Completed the development of the title bar and navigation bar.

---
+++ VideoManager/ViewModels/MainViewViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoManager.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        public DelegateCommand<object> NavCommand { get; set; }
+
+        private object pageContent;
+        public object PageContent { set => SetProperty(ref pageContent, value); get => pageContent; }
+
+        public MainViewModel()
+        {
+            NavCommand = new DelegateCommand<object>(OnNavCommand);
+        }
+
+        private void OnNavCommand(object obj)
+        {
+            PageContent = obj;
+        }
+    }
+}
---
+++ VideoManager/Views/MainView.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VideoManager.Views
+{
+    /// <summary>
+    /// MainView.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModels.MainViewModel();
+        }
+
+        private void DragArea_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                WindowState = WindowState == WindowState.Maximized
+                    ? WindowState.Normal
+                    : WindowState.Maximized;
+            }
+            else
+            {
+                DragMove();
+            }
+        }
+
+       private void Minimize_Click(object sender, RoutedEventArgs e)
+{
+    this.WindowState = WindowState.Minimized;
+}
+
+private void Maximize_Click(object sender, RoutedEventArgs e)
+{
+    if (this.WindowState == WindowState.Maximized)
+    {
+        this.WindowState = WindowState.Normal;
+    }
+    else
+    {
+        this.WindowState = WindowState.Maximized;
+    }
+}
+
+private void Close_Click(object sender, RoutedEventArgs e)
+{
+    this.Close();
+}
+
+    }
+}
